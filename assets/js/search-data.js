3e704b74204c0c3e2d74d4d060aab496adc08d61
hyeongjinhyun <hyeongjinhyun@gmail.com>
2025-09-22 15:38:28 +0000

    Deploying to gh-pages from @ hyeongjinhyun/hyeongjinhyun.github.io@381f2abecb31d66dfbe300e3431d20a992e255dc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,8 +24,8 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          title: "Teaching",
+          description: "Courses and roles across Purdue University and Seoul National University (SNU).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
